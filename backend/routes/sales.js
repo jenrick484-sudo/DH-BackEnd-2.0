ed0e7e63dbdefc9658a658092bd6c73a33fa38d9
jenrick484-sudo <montelee282@gmail.com>
2026-04-16 15:27:03 +0800

    Update sales.js

--- backend/routes/sales.js
+++ backend/routes/sales.js
@@ -2,22 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const authRoutes = require("./routes/auth");
+const itemsRoutes = require("./routes/items");
+const salesRoutes = require("./routes/sales");
+const dashboardRoutes = require("./routes/dashboard");
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const itemsRoutes = require("./routes/items");
-const salesRoutes = require("./routes/sales");
-const dashboardRoutes = require("./routes/dashboard");
-
-app.use("/api/items", itemsRoutes);
-app.use("/api/sales", salesRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+app.use("/api", authRoutes);
+app.use("/api", itemsRoutes);
+app.use("/api", salesRoutes);
+app.use("/api", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.send("Daiho Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running"));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
