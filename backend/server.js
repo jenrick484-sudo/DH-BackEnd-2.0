ab4fff8dd9db51630f6cbc72b59c1138e41a0b41
jenrick484-sudo <montelee282@gmail.com>
2026-04-17 11:18:15 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const dashboardRoutes = require("./routes/dashboard");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(bodyParser.json());
 
 app.use("/api", authRoutes);
