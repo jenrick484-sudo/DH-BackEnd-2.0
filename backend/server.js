a744bc35f01ab1e37e4031018fdb0bd8de7161ab
jenrick484-sudo <montelee282@gmail.com>
2026-04-22 09:50:08 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
 app.use("/api", itemsRoutes);
 app.use("/api", salesRoutes);
